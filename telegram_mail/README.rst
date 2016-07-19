8cdaac82ae49b4817f86be548fb73914f9aa2c7d
ilyasProgrammer <rakhimkulov@it-projects.info>
2016-07-19 11:53:36 +0500

    [ADD] telegram mail command. [REF] handle_subscriptions

---
+++ telegram_mail/README.rst
@@ -0,0 +1,15 @@
+===============
+ Telegram mail
+===============
+
+
+Further information
+-------------------
+
+HTML Description: https://apps.odoo.com/apps/modules/9.0/telegram
+
+Usage instructions: `<doc/index.rst>`_
+
+Changelog: `<doc/changelog.rst>`_
+
+Tested on Odoo 9.0 d3dd4161ad0598ebaa659fbd083457c77aa9448d
---
+++ telegram_mail/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Telegram mail command""",
+    "summary": """Subscribe to incoming odoo mail""",
+    "category": "Web",
+    "images": [],
+    "version": "1.0.0",
+
+    "author": "IT-Projects LLC",
+    "website": "https://it-projects.info",
+    "license": "GPL-3",
+    #"price": 9.00,
+    #"currency": "EUR",
+
+    "depends": [
+        "telegram",
+    ],
+    "external_dependencies": {},
+    "data": [
+        "data/ir_action_rules.xml",
+        "data/command.xml",
+    ],
+    "qweb": [
+    ],
+    "demo": [
+    ],
+
+    "installable": True,
+    "auto_install": False,
+
+}
